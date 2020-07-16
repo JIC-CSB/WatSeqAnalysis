c4d35d28e5463e37086e7d72e5bdac742cb89cb1
Ricardo H Ramirez-Gonzalez <ricardo.ramirez-gonzalez@jic.ac.uk>
2020-07-16 14:46:18 +0100

    The code to convert the tables for SQLite now ensures that the regions are consistent

--- normalized_cnv/02.convert_coverage_table_to_sqlite/meltMat.rb
+++ normalized_cnv/02.convert_coverage_table_to_sqlite/meltMat.rb
@@ -51,22 +51,48 @@ def each_region(mat_path, df_path, cov_path)
 
 
 	raise Exception.new "Columns not in the same order" unless compare_names(names, cov_names) 
-	i = 1
+	i = 0
+	j = 0
+	k = 0
+
+	cov_line = nil
+	df_line  = nil
+
 	while true
 		begin
-			df_line = df_each.next.gsub("\"","").split(",")
-			mat_line = mat_each.next.gsub("\"","").split(",").drop(1)
-			cov_line = cov_each.next.split("\t").drop(1)
-			yield names, mat_line, df_line, cov_line, i
+			
+			mat_line = mat_each.next.chomp.gsub("\"","").split(",")
+			mat_reg = mat_line[0]
+			mat_line = mat_line.drop(1)
+
+			loop do 
+				df_line = df_each.next.chomp.gsub("\"","").split(",")
+				df_reg = df_line[4]  
+				j += 1
+				break if df_reg == mat_reg
+			end
+			raise "mat_reg != df_line ( #{mat_reg} != #{df_line[4]})"  if mat_reg != df_line[4]  
+			
+			loop do
+				cov_line = cov_each.next.chomp.split("\t")
+				cov_reg = cov_line[0]
+				#puts "#{df_line[4]}\t#{mat_reg}\t#{cov_reg}"
+				#puts "Skipping: #{cov_reg} != #{mat_reg}"	if cov_reg != mat_reg
+				k += 1
+				break if cov_reg == mat_reg
+			end 
+			cov_line = cov_line.drop(1)
 			i += 1
-			#break if i > 10
+			yield names, mat_line, df_line, cov_line, i
+			
+			#break if i > 100
 		rescue StopIteration
 	  		$stderr.puts "Done iteration"
 	  		break
 	  	end
 	end
 	
-  	puts "EXITING REGION"
+  	puts "Mat: #{i}\nDF: #{j}\nCov: #{k}"
   	cov_io.close
 	mat_io.close
 	df_io.close
@@ -80,21 +106,15 @@ last_names = []
 df_out = File.open("#{out_path}/windows.bed","w")
 df_out.puts ["chrom", "chromStart", "chromEnd", "reg_id", "sd"].join("\t")
 
-norm_covs_out = File.open("#{out_path}/norm_covs.tsv", "w")
-norm_covs_out.puts ["reg_id", "line_id", "norm_cov"].join("\t")
 
 covs_out = File.open("#{out_path}/covs.tsv", "w")
-covs_out.puts ["reg_id", "line_id", "norm_cov"].join("\t")
+covs_out.puts ["reg_id", "line_id", "cov", "norm_cov"].join("\t")
 
 each_region(mat_path, df_path, cov_path) do |names, mat, df, cov, i|
 	df_out.puts [df[1], df[2], df[3], i, df[6]].join("\t") 
 
 	cov.each_with_index do |val, j|
-		covs_out.puts [i, j+1, val].join("\t")
-	end
-
-	mat.each_with_index do |val, j|
-		norm_covs_out.puts [i, j+1, val].join("\t")
+		covs_out.puts [i, j+1, val, mat[j]].join("\t")
 	end
 
 	#puts names.first(5).join("\t")
@@ -105,7 +125,7 @@ end
 
 covs_out.close
 df_out.close
-norm_covs_out.close
+
 
 lines_out = File.open("#{out_path}/lines.tsv", "w")
 lines_out.puts ["line_id", "line"].join("\t")
