40b34520777ef3ee2ab369b8d7c5b103a33d3063
Ricardo H Ramirez-Gonzalez <ricardo.ramirez-gonzalez@jic.ac.uk>
2020-07-17 09:24:22 +0100

    Notebook updated to plot with the new table

--- normalized_cnv/02.convert_coverage_table_to_sqlite/load_tables.sh
+++ normalized_cnv/02.convert_coverage_table_to_sqlite/load_tables.sh
@@ -4,5 +4,5 @@ echo $DIR
 sqlite3 covs_200bp.db < $DIR/create_database.sql
 gunzip -c lines.tsv.gz  | sqlite3   -csv -separator "	" covs_200bp.db '.import /dev/stdin lines'
 gunzip -c windows.bed.gz  | sqlite3 -csv -separator "	" covs_200bp.db '.import /dev/stdin regions'
-gunzip -c norm_covs.tsv.gz  | sqlite3 -csv -separator "	" covs_200bp.db '.import /dev/stdin norm_covs'
+#gunzip -c norm_covs.tsv.gz  | sqlite3 -csv -separator "	" covs_200bp.db '.import /dev/stdin norm_covs'
 gunzip -c covs.tsv.gz  | sqlite3 -csv -separator "	" covs_200bp.db '.import /dev/stdin covs'
