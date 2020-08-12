cdabad154ec08659829c8db9e788afa723874ace
Ricardo H Ramirez-Gonzalez <ricardo.ramirez-gonzalez@jic.ac.uk>
2020-08-12 19:23:36 +0100

    Plot now works to show cov and norm_cov.

--- normalized_cnv/bio.cnv/R/getRegionFromDB.r
+++ normalized_cnv/bio.cnv/R/getRegionFromDB.r
@@ -1,11 +1,6 @@
 getRegionFromDB <- function(covs_db, rename=TRUE, region = null ){
     query <- "SELECT 
-chrom, 
-chromStart as start, 
-chromEnd as end, 
-reg_id, 
-chromEnd - chromStart as length, 
-sd as sd_region 
+*
 FROM 
 regions "
 
@@ -13,12 +8,12 @@ regions "
         chr = seqnames(region)[1]
         first = start(region)[1]
         last = end(region)[1] 
-        query <- paste0(query, "WHERE chrom = '", chr, "' and start BETWEEN ", first, " AND ",last )       
+        query <- paste0(query, " WHERE chrom = '", chr, "' and chromStart BETWEEN ", first, " AND ",last )       
     }
     res <- dbSendQuery(covs_db,query)
     df  <- dbFetch(res, n = -1)
     if(rename){
-        colnames(df) <- c("Scaffold", "Start", "Ends", "Exon", "ExonL", "sdExon")
+        colnames(df) <- c("chrom", "start", "end", "reg_id", "sd")
     }
     df 
 }
--- normalized_cnv/bio.cnv/R/plotRegionScatterLines.r
+++ normalized_cnv/bio.cnv/R/plotRegionScatterLines.r
@@ -1,26 +1,52 @@
-plotRegionScatterLines <- function(covs_db,gff, region, lines=c(), window_size=250000, levels_count = 10 ){
+plotRegionScatterLines <- function(covs_db,gff, region, show.ribons=F, genes=c(),  lines=c(), norm_cov_range = c(0,3), window_size=250000, levels_count = 10, show.coverage = FALSE  ){
     local_cov <- getWindowsInRange(covs_db,  ranges=region)
     gff_local <-  subsetByOverlaps(gff, region)
     gff_local <- gff_local[gff_local$type == "gene"]
     gff_local$pos_id <- seq(from = 0, to=length(gff_local)-1)
     gff_local$ymax <- 0.5 + (gff_local$pos_id %% levels_count)
     gff_local$ymin <- gff_local$pos_id %% levels_count
+    gff_local$highligthed <- gff_local$gene_id %in% genes
 
-    p.norm.cov  <- ggplot(local_cov, aes(y=norm_cov, x=start, group= window_size * round(start / window_size) )) 
-    p.norm.cov  <- p.norm.cov + theme_bw() + geom_boxplot(outlier.shape = NA) 
+    gff_h <- gff_local[gff_local$highligthed]
+
+
+    p.norm.cov  <- ggplot() + geom_vline(xintercept=start(gff_h), col="red")
+    p.norm.cov  <- p.norm.cov + theme_bw()
+
+    #regions     <- data.frame( getRegionFromDB(covs_db, region=region, rename=FALSE) )
+    if(show.ribons){
+        regions     <- getRegionFromDB(covs_db, region=region, rename=TRUE) 
+        regions$y_min = 1 - (2* regions$sd )
+        regions$y_max = 1 + (2* regions$sd )
+
+        #y_min = 0.5
+        #y_max = 1.5
+        p.norm.cov  <- p.norm.cov  + geom_ribbon(data = regions, mapping=aes(x = start ,ymin = y_min, ymax = y_max), fill = "grey70")
+        #return(head(regions));
+    }else{
+         p.norm.cov <- p.norm.cov + geom_boxplot(data=data.frame(local_cov), mapping=aes(y=norm_cov, x=start, group= window_size * round(start / window_size) ), outlier.shape = NA) 
+    }
     p.norm.cov  <- p.norm.cov + theme(legend.position = "top")
-    p.norm.cov  <- p.norm.cov + ylim(c(0,3))
+    p.norm.cov  <- p.norm.cov + ylim(norm_cov_range)
 
     p.genes <- ggplot(gff_local) + geom_rect(aes(xmin=start, xmax=end , ymin = ymin, ymax = ymax ), stat = "identity", color = NA) 
-    p.genes <- p.genes +  geom_text(aes(x=start, y =  ymax+0.2, label = gene_id), size=2, check_overlap=T ) 
-    p.genes <- p.genes + theme_bw()
+    p.genes <- p.genes +  geom_text(aes(x=start, y =  ymax+0.2, label = gene_id, color=highligthed), size=2, check_overlap=T ) 
+    p.genes <- p.genes + theme_bw() + theme(legend.position = "none") + scale_colour_manual(values=c("TRUE"="red", "FALSE"="black"))
 
     heights = c()
     tracks_details <- list()
 
     for(line in lines){
         tmp_data <- data.frame(local_cov[local_cov$line == line])
-        tracks_details[[line]] <-  p.norm.cov  + geom_point(data=(tmp_data), shape=".")
+        
+        if(show.coverage){
+            title_cov <- paste0(line, " cov")
+             tracks_details[[title_cov]] <- ggplot()  + geom_point(data=(tmp_data), mapping = aes(x=start, y=cov),shape=".") + theme_bw()
+             heights <- append(heights,1)
+        }
+       
+        
+        tracks_details[[line]] <-  p.norm.cov  + geom_point(data=(tmp_data), mapping = aes(x=start, y=norm_cov),shape=".")
         heights <- append(heights,1)
     }
 
@@ -30,4 +56,5 @@ plotRegionScatterLines <- function(covs_db,gff, region, lines=c(), window_size=2
     tks <- tracks(tracks_details, heights = heights )  + xlim(gr4D)  + scale_x_sequnit(unit = "Mb" ) 
     tks
     #gff_local
+    #regions
 }
